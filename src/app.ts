08d32a8040a04a9b3edeadcced0779a165d891df
camory <christophe@amory.fr>
2016-04-20 10:45:50 +0200

    added 1st component

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
 import { bootstrap } from 'angular2/platform/browser'
+import { Ng2CodelabApp } from './app/ng2-codelab';
 
-// import and bootstrap you angular2 application...
+bootstrap(Ng2CodelabApp,[]);
---
+++ src/app/ng2-codelab.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: 'app/ng2-codelab.html'
+})
+export class Ng2CodelabApp {
+  private title: string = 'Angular 2 Quiz App';
+}
